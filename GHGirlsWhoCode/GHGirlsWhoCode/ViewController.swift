b4de3ab402048d9923c307312dd12427258fadac
Olga Dimitrijevic <odimitrijevic@grubhub.com>
2018-07-25 12:36:10 -0500

    add code

---
+++ GHGirlsWhoCode/GHGirlsWhoCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GHGirlsWhoCode
+//
+//  Created by Olga Dimitrijevic on 7/25/18.
+//  Copyright © 2018 Olga Dimitrijevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
